c83245eceb2c413042ef673cd1225b435e7e6b48
appbak3r <ad@researchinstitute.io>
2019-11-16 19:04:23 +0300

    Updates

---
+++ precache-manifest.5ee44fb4dead8cbc0dab20fb7053ab92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1976180efffad5b62dd8dec5dfdfedf4",
+    "url": "/cloudike/index.html"
+  },
+  {
+    "revision": "7e3864acd8debc44c018",
+    "url": "/cloudike/static/js/2.f551fadd.chunk.js"
+  },
+  {
+    "revision": "3fea08521de879c82f8f",
+    "url": "/cloudike/static/js/main.d93f2e72.chunk.js"
+  },
+  {
+    "revision": "a07b4170501bfc7b8b52",
+    "url": "/cloudike/static/js/runtime-main.c9e16aa4.js"
+  },
+  {
+    "revision": "64559e048513438998d3d6b3900d8251",
+    "url": "/cloudike/static/media/logo.64559e04.svg"
+  },
+  {
+    "revision": "97c55415035f15d5a8af95508c824a23",
+    "url": "/cloudike/static/media/spinner.97c55415.svg"
+  }
+]);
